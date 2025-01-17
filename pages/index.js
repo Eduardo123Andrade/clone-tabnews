940ae60020c3f5e383064c190f7384deaf842c17
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-01-17 00:30:04 +0000

    adicioan scripts para lint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-const Home = ( ) => {
-    return (
-        <h1>
-            Olar, pessoinha
-        </h1>
-    )
-}
+const Home = () => {
+  return <h1>Olar, pessoinha</h1>;
+};
 
+const test = () => {
+  console.log("olar");
+};
 
-export default Home
+export default Home;
