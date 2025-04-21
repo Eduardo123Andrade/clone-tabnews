a16f473fa0502c92ba55a8c67f38e50333345977
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-04-21 16:02:18 +0000

    removed console.log

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(args) {
     ssl: process.env.NODE_ENV !== 'development'
   });
 
-  console.log("Credenciais do Postgress", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
   try {
     await client.connect();
     const result = await client.query(args);
